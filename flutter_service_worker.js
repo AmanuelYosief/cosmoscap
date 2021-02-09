9fd61566c569fb0588f2d4f69f2fcc891dbb3234
AmanuelYosief <43088213+AmanuelYosief@users.noreply.github.com>
2021-02-09 23:06:32 +0000

    web-renderer canvaskit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "749e4c25953adb9ef3c3fdde14765b7c",
-"/": "749e4c25953adb9ef3c3fdde14765b7c",
-"main.dart.js": "e6ab514f2208513daa271aec1d6182c7",
+"index.html": "fda6e79ea4f799385d087e1ce42d39b1",
+"/": "fda6e79ea4f799385d087e1ce42d39b1",
+"main.dart.js": "26d5f062ca8784a744868a37fe1f1d54",
 "manifest.json": "d7bfaad09f7ff20cc690dc633f8d2bb9",
 "version.json": "2681139d39c8e254d57360c4f9765972"
 };
