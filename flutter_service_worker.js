c958b97faff3bb20be4c32b0c8e90eabbecf51e5
Amanuel Yosief Mussie <43088213+AmanuelYosief@users.noreply.github.com>
2022-03-25 00:24:39 +0000

    listed $regen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,18 +9,22 @@ const RESOURCES = {
 "assets/assets/secretSwapPromo.png": "ec4272fd6a2cc348fac982a28b786afc",
 "assets/assets/stashh.png": "d78c7e4c7f9d4eb95c0c13cdb4d698ee",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "e7da3d4b70b5c9be70851d57f70c4d98",
+"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
+"assets/NOTICES": "19e5046d1fbc9961dc216a72724a8c07",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "cosmos.png": "fd93ddb7243853c5173e3bc865bc6d3e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "aab6fde38ba3b5c134edb24b24d0bace",
-"/": "aab6fde38ba3b5c134edb24b24d0bace",
-"main.dart.js": "c974161dc9eca55f188bf99be787afad",
+"index.html": "48eb0a55cf7894e7c1a4b84eecf2303c",
+"/": "48eb0a55cf7894e7c1a4b84eecf2303c",
+"main.dart.js": "3b309d62a81bee18ac8d67c0b992d481",
 "manifest.json": "d7bfaad09f7ff20cc690dc633f8d2bb9",
-"version.json": "2681139d39c8e254d57360c4f9765972"
+"version.json": "8e4fdcaf63463a550a2e1cf4b059e025"
 };
 
 // The application shell files that are downloaded before a service worker can
